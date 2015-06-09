738046a803eb41f4791ac7ccd85892050e1932ab
Bruce Wade <bruce.wade@gmail.com>
2015-06-08 18:31:06 -0700

    Reorganized project, added different settings for environments.

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""django_dating URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
