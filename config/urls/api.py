94fa5add5c291a41154f259a562bdf3740c9ec3a
Bruce Wade <bruce@lingq.com>
2015-10-06 18:18:10 -0700

    Removed import not used

--- config/urls/api.py
+++ config/urls/api.py
@@ -4,7 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
 from dating_project.users.api import viewsets as user_api_views
-from dating_project.board import views as board_api_views
 
 router = DefaultRouter()
 router.register(r'users', user_api_views.UserViewSet)
